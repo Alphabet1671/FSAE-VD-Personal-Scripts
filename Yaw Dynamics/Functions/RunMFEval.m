ac44fdcc4cfa6b23cfda3ddd278e0c48bfe5d320
Alphabet1671 <Alphabet1671@foxmail.com>
2026-02-05 20:55:38 -0600

    Working mfeval block, somewhat correct results, huge problem with relaxation length calcs

---
+++ Yaw Dynamics/Functions/RunMFEval.m
@@ -0,0 +1,15 @@
+function [Fx, Fy, My, Mz, sigmax, sigmay]= RunMFEval(Fz, alpha, kappa, gamma, vx)
+
+tire = mfeval.readTIR('Hoosier43075_16x75_10_R20_7_mod2_COMBINED.tir');
+inputs = [Fz, kappa, alpha, gamma, 0, vx];
+
+outputs = mfeval(tire, inputs, 121);
+
+Fx = outputs(1);
+Fy = outputs(2);
+Mz = outputs(6);
+My = outputs(5);
+sigmax = outputs(27);
+sigmay = outputs(28);
+
+end
